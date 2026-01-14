da857dcd7a9f873352cc8b780da8c61ea79f41b0
VITTALPRABHU07 <vittalprabhu2703@gmail.com>
2026-01-14 23:45:46 +0530

    Update Read and print a matrix.c

--- arrays/Multidimensional Arrays (2D Arrays)/Read and print a matrix/Read and print a matrix.c
+++ arrays/Multidimensional Arrays (2D Arrays)/Read and print a matrix/Read and print a matrix.c
@@ -1,1 +1,32 @@
 //Read and print a matrix
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j;
+    int mat[10][10];
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("\nMatrix elements are:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
