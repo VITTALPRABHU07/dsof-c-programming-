47ff4c5385f2159a0d5ef4e6f00c553af57b574f
VITTALPRABHU07 <vittalprabhu2703@gmail.com>
2026-01-17 22:23:16 +0530

    Create Transpose of the matrix.c

---
+++ arrays/Multidimensional Arrays (2D Arrays)/Matrix transpose/Transpose of the matrix.c
@@ -0,0 +1,41 @@
+//Transpose of the matrix
+#include <stdio.h>
+
+int main()
+{
+    int r, c, i, j;
+    int a[10][10], t[10][10];
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Transpose logic
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+        {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
